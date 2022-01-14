f87484910070b0aa2309db453404ab83690419fa
UnaiRM <miperritolukas@gmail.com>
2022-01-14 09:52:17 +0100

    Diccionarios
    Diccionarios de users, characters, adcentures, steps y options

--- M3/usuario.py
+++ M3/usuario.py
@@ -2,13 +2,13 @@ import pymysql
 #ESTABLECE CONEXION
 conn = pymysql.connect(host='52.178.107.50', user='xavi', password='12345678', db='BDDPROYECTOENERO')
 
-#CONSULTA
+
+
+#Diccionario Usuarios
+
 cur = conn.cursor()
 query = 'select * from USER'
-
 cur.execute(query)
-
-#FETCHALL
 rows = cur.fetchall()
 
 users = {}
@@ -17,9 +17,76 @@ for id in range(1,len(rows)+1):
     for item in rows[id-1]:
         infoUsuario.append(item)
     users[id] = infoUsuario[1:]
-
-print(users[1][0])
-
+print(users)
 
 
 
+#Diccionario personajes
+
+query = 'select * from BDDPROYECTOENERO.CHARACTER'
+cur.execute(query)
+rows = cur.fetchall()
+
+characters = {}
+for id in range(1,len(rows)+1):
+    infoCharacter = []
+    for item in rows[id-1]:
+        infoCharacter.append(item)
+    characters[id] = infoCharacter[1:]
+
+print(characters)
+
+
+
+#Diccionario Aventuras
+
+query = 'select * from ADVENTURE'
+cur.execute(query)
+rows = cur.fetchall()
+
+adventures = {}
+for id in range(1,len(rows)+1):
+    infoAdventure = []
+    for item in rows[id-1]:
+        infoAdventure.append(item)
+    adventures[id] = infoAdventure[1:]
+
+print(adventures)
+
+
+
+#Diccionario Steps
+
+query = 'select * from STEP'
+cur.execute(query)
+rows = cur.fetchall()
+
+steps = {}
+for id in range(1,len(rows)+1):
+    infoStep = []
+    for item in rows[id-1]:
+        infoStep.append(item)
+    steps[id] = infoStep[1:]
+
+print(steps)
+
+
+
+#Diccionario Options
+
+query = 'select * from BDDPROYECTOENERO.OPTION'
+cur.execute(query)
+rows = cur.fetchall()
+
+options = {}
+for id in range(1,len(rows)+1):
+    infoOption = []
+    for item in rows[id-1]:
+        infoOption.append(item)
+    options[id] = infoOption[1:]
+
+print(options)
+
+
+
+#Diccionario
